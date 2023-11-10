edd42713c8ebc9938abae6ede8276071497ff93f
daniil <dannydendi@gmail.com>
2023-11-11 01:18:51 +0200

    Update

--- src/main/java/GameField.java
+++ src/main/java/GameField.java
@@ -7,31 +7,28 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
-    private final int SIZE = 320;
-    private final int DOT_SIZE = 16;
-    private final int ALL_DOTS = 400;
+    private static final int SIZE = 320;
+    private static final int DOT_SIZE = 16;
+    private static final int ALL_DOTS = 400;
+    private static final int GAME_OVER_X = 125;
+    private static final int GAME_OVER_Y = SIZE / 2;
+
     private Image snake;
     private Image mouse;
     private int mouseX;
     private int mouseY;
     private int[] x = new int[ALL_DOTS];
     private int[] y = new int[ALL_DOTS];
-
     private int dots;
-
     private Timer timer;
-
-    private boolean left = false;
-
-    private boolean right = true;
-
-    private boolean up = false;
-
-    private boolean down = false;
-
     private boolean inGame = true;
 
-    public GameField(){
+    private enum Direction { UP, DOWN, LEFT, RIGHT }
+
+    private Direction currentDirection = Direction.RIGHT;
+    private Random random = new Random();
+
+    public GameField() {
         setBackground(Color.white);
         loadImages();
         initGame();
@@ -39,117 +36,107 @@ public class GameField extends JPanel implements ActionListener {
         setFocusable(true);
     }
 
-    public void initGame(){
+    public void initGame() {
         dots = 3;
         for (int i = 0; i < dots; i++) {
-            x[i] = 48 - i*DOT_SIZE;
+            x[i] = 48 - i * DOT_SIZE;
             y[i] = 48;
         }
-        timer = new Timer(250,this);
+        timer = new Timer(250, this);
         timer.start();
         createApple();
     }
 
-    public void createApple(){
-        mouseX = new Random().nextInt(20)*DOT_SIZE;
-        mouseY = new Random().nextInt(20)*DOT_SIZE;
+    public void createApple() {
+        mouseX = random.nextInt(20) * DOT_SIZE;
+        mouseY = random.nextInt(20) * DOT_SIZE;
     }
-    public void loadImages(){
-        ImageIcon iia = new ImageIcon("mouse.png");
+
+    public void loadImages() {
+        ImageIcon iia = new ImageIcon(getClass().getResource("/mouse.png"));
         mouse = iia.getImage();
-        ImageIcon iib = new ImageIcon("snake.png");
+        ImageIcon iib = new ImageIcon(getClass().getResource("/snake.png"));
         snake = iib.getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (inGame){
-            g.drawImage(mouse,mouseX,mouseY,this);
+        if (inGame) {
+            g.drawImage(mouse, mouseX, mouseY, this);
             for (int i = 0; i < dots; i++) {
-                g.drawImage(snake,x[i],y[i],this);
+                g.drawImage(snake, x[i], y[i], this);
             }
-        }else{
+        } else {
             String str = "Game Over";
             g.setColor(Color.BLACK);
-            g.drawString(str,125,SIZE/2);
+            g.drawString(str, GAME_OVER_X, GAME_OVER_Y);
         }
     }
 
-    public void move(){
+    public void move() {
         for (int i = dots; i > 0; i--) {
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
-        if (left){
+        if (currentDirection == Direction.LEFT) {
             x[0] -= DOT_SIZE;
-        }if (right){
+        }
+        if (currentDirection == Direction.RIGHT) {
             x[0] += DOT_SIZE;
-        }if (up){
+        }
+        if (currentDirection == Direction.UP) {
             y[0] -= DOT_SIZE;
-        }if (down){
+        }
+        if (currentDirection == Direction.DOWN) {
             y[0] += DOT_SIZE;
         }
     }
-    public void checkApple(){
-        if (x[0] == mouseX && y[0] == mouseY){
+
+    public void checkApple() {
+        if (x[0] == mouseX && y[0] == mouseY) {
             dots++;
             createApple();
         }
     }
-    public void checkCollisions(){
+
+    public void checkCollisions() {
         for (int i = dots; i > 0; i--) {
-            if (i>4 && x[0] == x[i] && y[0] == y[0]){
+            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
                 inGame = false;
             }
         }
-        if (x[0] > SIZE){
-            inGame = false;
-        }
-        if (x[0] < 0){
-            inGame = false;
-        }
-        if (y[0] > SIZE){
-            inGame = false;
-        }
-        if (y[0] < 0){
+        if (x[0] > SIZE || x[0] < 0 || y[0] > SIZE || y[0] < 0) {
             inGame = false;
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (inGame){
+        if (inGame) {
             checkApple();
             checkCollisions();
             move();
         }
         repaint();
     }
-    class FieldKeyListener extends KeyAdapter{
+
+    class FieldKeyListener extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
             super.keyPressed(e);
             int key = e.getKeyCode();
-            if (key == KeyEvent.VK_LEFT && !right){
-                left = true;
-                up = false;
-                down = false;
+            if (key == KeyEvent.VK_LEFT && currentDirection != Direction.RIGHT) {
+                currentDirection = Direction.LEFT;
             }
-            if (key == KeyEvent.VK_RIGHT && !left) {
-                right = true;
-                up = false;
-                down = false;
+            if (key == KeyEvent.VK_RIGHT && currentDirection != Direction.LEFT) {
+                currentDirection = Direction.RIGHT;
             }
-            if (key == KeyEvent.VK_UP && !down) {
-                right = false;
-                up = true;
-                left = false;
+            if (key == KeyEvent.VK_UP && currentDirection != Direction.DOWN) {
+                currentDirection = Direction.UP;
             }
-            if (key == KeyEvent.VK_DOWN && !up) {
-                right = false;
-                down = true;
-                left = false;
+            if (key == KeyEvent.VK_DOWN && currentDirection != Direction.UP) {
+                currentDirection = Direction.DOWN;
             }
         }
     }
